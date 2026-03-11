3b7c73c14dde2a69cfd282020ca1917947f0cf4b
Maha <maharagav20@gmail.com>
2026-03-11 12:02:11 +0530

    [maha]: UC5: Added API to find greeting by ID

--- greetingapp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ greetingapp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -26,4 +26,8 @@ public class GreetingController {
     public Greeting saveGreeting(@RequestParam String message) {
         return greetingService.saveGreeting(message);
     }
+    @GetMapping("/{id}")
+    public Greeting getGreetingById(@PathVariable Long id) {
+        return greetingService.getGreetingById(id);
+    }
 }
